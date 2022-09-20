0b2b4f97a726bb666b9f9f6b06094b2b6172ab93
Irina <1303irina1993lagutina@mail.ru>
2022-09-21 01:06:30 +0700

    Сохранение заданий

--- Program.cs
+++ Program.cs
@@ -32,3 +32,132 @@ void PrintArray(int[] numbers)
     Console.Write("]");
     Console.WriteLine();
 }
+
+
+//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Вот наш массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int z = 0; z < numbers.Length; z+=2)
+    sum = sum + numbers[z];
+
+    Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+
+
+//Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+//[1 2 3 4 5] -> 5 8 3
+//[6 7 3 6] -> 36 21
+
+int[] GetPairMult (int[] array)
+{
+    int size = 0;
+    int result = null;
+
+if (array.Length % 2 == 0)
+{
+    size = array.Length / 2;
+    result = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        result[i] = array[i] + array[array.Length - i - 1];
+    }
+}
+else
+{
+    size = (array.Length/2) + 1;
+    result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        if (i == (size - 1))
+        {
+            result[i] = array[i];
+        }
+        else
+        {
+            result[i] = array[i] * array[array.Length - i - 1];
+        }
+    }
+}
+
+return result;
+}
+
+int[] array == new int[] { 1, 2, 3, 4, 5 };
+int[] result = GetPairMult(array);
+Console.WriteLine("");
+
+
+
+//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int z = 0; z < numbers.Length; z++)
+{
+    if (numbers[z] > max)
+        {
+            max = numbers[z];
+        }
+    if (numbers[z] < min)
+        {
+            min = numbers[z];
+        }
+}
+
+Console.WriteLine($"Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
